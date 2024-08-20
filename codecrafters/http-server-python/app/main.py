357ba5462a72dbb7efae8aaf3fc0338d345dbc96
eukrateia <32756828+eukrateia@users.noreply.github.com>
2024-08-20 00:03:37 -0400

    Implement full HTTP server requests

--- codecrafters/http-server-python/app/main.py
+++ codecrafters/http-server-python/app/main.py
@@ -1,56 +1,160 @@
+import sys
 import socket
+import threading
 
-# Handles an incoming HTTP request by sending a 200 OK response
-def handle_request(client_socket):
+# Config
+HOST = "localhost"
+PORT = 4221
+FILES_DIR = ""
 
-    # read data from the client
-    client_socket.recv(1024) # reading data
+# HTTP Statuses
+HTTP_STATUS_OK = "HTTP/1.1 200 OK\r\n"
+HTTP_STATUS_NOT_FOUND = "HTTP/1.1 404 Not Found\r\n"
+HTTP_STATUS_CREATED = "HTTP/1.1 201 Created\r\n"
 
-    # send a 200 OK reponse
-    response = "HTTP/1.1 200 OK\r\n\r\n"
-    client_socket.send(response.encode())
 
-def main():
+def response(status, headers, body):
+    """
+    Returns a properly formatted HTTP response.
 
-    # You can use print statements as follows for debugging, they'll be 
-    #   visible when running tests.
-    log_x = 1
-    print(f"{log_x} Log_10: *** Begin main() ***")
+    Args:
+        status (str): The HTTP status code and message.
+        headers (dict): A dictionart of HTTP headers.
+        body (str): The response body.
 
-    host_ip_listen = "localhost"
-    host_port_listen = 4221
+    Returns:
+        bytes: The encoded HTTP response.
+    """
+    return (status +
+            "".join(f"{k}: {v}\r\n" for k, v in headers.items()) +
+            "\r\n" +
+            body + "\r\n").encode()
 
-    # create a TCP/IP socket and bind to port 4221
-    server_socket = socket.create_server(
-            (host_ip_listen, host_port_listen), 
-            reuse_port=True
+
+def handle_request(request, files=None):
+    """
+    Handle incoming HTTP requests and generate appropriate HTTP responses.
+
+    Args:
+        request (list): A list containing the HTTP request data.
+        files (str, optional): the path to the directory containing
+                               files to be served. Defaults to None.
+
+    Returns:
+        response: An HTTP response object containing
+        the appropriate status code, headers, and body.
+    """
+    method, path, _ = request[0].split(" ")
+
+    if method == "GET" and path == "/":
+        res = response(
+            status=HTTP_STATUS_OK + "\r\n",
+            headers={},
+            body=""
         )
-    log_x += 1
-    print(f"{log_x} Log_20: Server is running on port {host_port_listen}")
 
-    try:
-        while True:
-            # wait for a connection
-            log_x += 1
-            print(f"{log_x} Log_30: Waiting for a connection...")
-            client_socket, addr = server_socket.accept()
+    elif method == "GET" and path.startswith('/echo'):
+        content = path.split("/echo/")[1]
+        res = response(
+            status=HTTP_STATUS_OK,
+            headers={"Content-Type": "text/plain",
+                     "Content-Length": len(content)},
+            body=content
+        )
 
-            log_x += 1
-            print(f"{log_x} Log_40: Connection from {addr} has been established")
+    elif method == "GET" and path.starswith('/user-agent'):
+        content = request[2].split(": ")[1]
+        res = response(
+            status=HTTP_STATUS_OK,
+            headers={"Content-Type": "text/plain",
+                     "Content-Length": len(content)},
+            body=content
+        )
 
-            # handle the client request
-            handle_request(client_socket)
+    elif path.startswith('/files'):
+        file_name = path.split("/files/")[1]
+        if method == "POST":
+            content = request[-1]
+            handle_files(
+                file_name=file_name,
+                file_content=content,
+                mode="write"
+            )
 
-            # close the connection to the client
-            client_socket.close()
-    except KeyboardInterrupt:
-        log_x += 1
-        print(f"\n{log_x} Log_50: Server is shutting down.")
-    finally:
-        # clean up the server socket
-        server_socket.close()
-        log_x += 1
-        print(f"{log_x} Log_60: Server has been shut down.")
+            res = response(
+                status=HTTP_STATUS_CREATED,
+                headers={"Content-Type": "text/plain",
+                         "Content-Length": len(content)},
+                body=content
+            )
+        else:
+            file_content = handle_files(
+                file_name=file_name,
+                mode="read"
+            )
+            if file_content:
+                res = response(
+                    status=HTTP_STATUS_OK,
+                    headers={"Content-Type": "text/plain",
+                            "Content-Length": len(file_content)},
+                    body=file_content
+                )
+            else:
+                res = response(
+                    status=HTTP_STATUS_NOT_FOUND + "\r\n",
+                    headers={},
+                    body=""
+                )
+            return res
+
+
+def handle_files(file_name, file_content=None, mode="read"):
+    if mode == 'write':
+        with open(f"{FILES_DIR}{FILE_NAME}", "w", encoding='UTF8') as f:
+            file = f.write(file_content)
+
+    else:
+        try:
+            with open(f"{FILES_DIR}{file_name}", "r") as f:
+                file = f.read()
+        except FileNotFoundError:
+            file = None
+
+    return file
+
+
+def handle_client(conn, addr):
+    """
+    Handle incoming client requests.
+
+    Args:
+        conn (socket): The client socket connection.
+        addr (tuple): The client address.
+
+    Returns:
+        bytes: The response to send back to the client.
+    """
+    request = conn.recv(4096).decode(
+        ).splitlines()
+    
+    conn.send(handle_request(request))
+    conn.close()
+
+
+def main(args):
+    server_socket = socket.create_server((HOST, PORT), reuse_port=True)
+    if args:
+        global FILES_DIR
+        FILES_DIR = args[1]
+
+    while True:
+        conn, addr = server_socket.accept() # wait for client
+
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+
+        thread.start()
+
+
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
